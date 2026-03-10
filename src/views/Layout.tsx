9f57285ac07f98d165aeca1285d65da4c154f051
aek676 <aek676@inlumine.ual.es>
2026-03-10 19:28:44 +0100

    feat(layout): switch to Minty theme and update Bootstrap CDN
    Changed the Bootswatch theme from Lumen to Minty for a refreshed look.
    Updated Bootstrap and Bootswatch CDN links to use jsdelivr with
    integrity
    checks. Ensured proper HTML structure in Layout component.

--- src/views/Layout.tsx
+++ src/views/Layout.tsx
@@ -9,20 +9,23 @@ export function Layout({ title, children }: LayoutProps) {
 			<head>
 				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 				<title>{title || "TRWM"}</title>
+
 				<link
 					rel="stylesheet"
-					href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/5.3.3/lumen/bootstrap.min.css"
+					href="https://cdn.jsdelivr.net/npm/bootswatch@4.5.2/dist/minty/bootstrap.min.css"
+					integrity="sha384-H4X+4tKc7b8s4GoMrylmy2ssQYpDHoqzPa9aKXbDwPoPUA3Ra8PA5dGzijN+ePnH"
+					crossorigin="anonymous"
 				/>
+
 				<link rel="stylesheet" href="/stylesheets/style.css" />
-			</head>
-			<body>
-				{children}
+
 				<script
 					src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
 					integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
 					crossorigin="anonymous"
 				/>
-			</body>
+			</head>
+			<body>{children}</body>
 		</html>
 	);
 }
